73ba13d91025f915cb54f1175940e3ea97675f3c
Misha <misha.dorozhkin@mail.ru>
2023-09-24 14:11:36 +0300

    Выполнена домашняя работа №3

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,10 +1,14 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-SELECT company_name as customer, CONCAT(first_name, ' ', last_name) AS employee
-FROM customers
-INNER JOIN employees USING (city)
-WHERE city = 'London'
+SELECT customers.company_name as customer, CONCAT(first_name, ' ', last_name) AS employee
+FROM orders
+INNER JOIN employees USING (employee_id)
+INNER JOIN customers USING (customer_id)
+INNER JOIN shippers ON orders.ship_via = shippers.shipper_id
+WHERE customers.city = 'London'
+AND employees.city = 'London'
+AND shippers.company_name = 'United Package'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
@@ -27,8 +31,13 @@ ORDER BY company_name
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT product_name
+FROM products
+WHERE product_name IN
+(
 SELECT DISTINCT product_name
 FROM products
 LEFT JOIN order_details USING (product_id)
 WHERE order_details.quantity = 10
+)
 
